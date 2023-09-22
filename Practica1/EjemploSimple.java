ea15f55da6bb7a2025ac6bd3c3a57d0e1ce477b7
Santiago Álvarez <A01640172@itesm.mx>
2023-09-22 11:26:25 +0200

    initial commit

---
+++ Practica1/EjemploSimple.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosimple;
+import java.io.File;
+import org.apache.tika.Tika;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.langdetect.optimaize.OptimaizeLangDetector;
+import org.apache.tika.language.detect.LanguageDetector;
+
+/**
+ *
+ * @author jhg
+ */
+public class EjemploSimple {
+
+    public static void main(String[] args) throws Exception {
+
+  // Creaamos una instancia de Tika con la configuracion por defecto
+  Tika tika = new Tika();
+  // Se parsean todos los ficheros pasados como argumento y se extrae el contenido
+  for (String file : args) {
+      File f = new File(file);
+      
+      String type = tika.detect(f);
+      System.out.println(file +":"+type);
+      
+     
+      String text = tika.parseToString(f);
+      System.out.print(text);
+      
+     
+  }
+
+  String[] textos={"Esto es un ejemplo de uso del detector de lenguaje",
+  "this is an example of language detector", 
+  "bon jour, mademoiselle"};
+  LanguageDetector detector = new OptimaizeLangDetector().loadModels();
+  for (int i =0;i<3;i++){  
+      detector.addText(textos[i]);
+      System.out.println(detector.detect().getLanguage());
+      detector.reset();
+  }
+
+  //System.out.println("Escrito en :"+detector.detect().getLanguage());
+  }
+    
+}
