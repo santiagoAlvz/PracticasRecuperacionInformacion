fda4faf05212111589b36025250cfd1d72edfbc5
eefthymiou <johnefthimiou1002@gmail.com>
2023-10-16 11:23:46 +0200

    filters added

--- Practica2/Practica2.java
+++ Practica2/Practica2.java
@@ -18,6 +18,8 @@ import java.util.Map;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.LowerCaseFilter;
 import org.apache.lucene.analysis.TokenStream;
@@ -32,6 +34,7 @@ import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.analysis.core.WhitespaceTokenizer;
 import org.apache.lucene.analysis.custom.CustomAnalyzer;
 import org.apache.lucene.analysis.ngram.EdgeNGramTokenFilter;
+import org.apache.lucene.analysis.ngram.NGramTokenFilter;
 import org.apache.lucene.analysis.pattern.PatternReplaceCharFilterFactory;
 import org.apache.lucene.analysis.pattern.SimplePatternSplitTokenizerFactory;
 import org.apache.lucene.analysis.shingle.ShingleFilter;
@@ -41,6 +44,14 @@ import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.core.KeywordAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.es.SpanishAnalyzer;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.StopFilter;
+import org.apache.lucene.analysis.snowball.SnowballFilter;
+import org.apache.lucene.analysis.commongrams.CommonGramsFilter;
+import org.apache.lucene.analysis.synonym.SynonymFilter;
+import org.apache.lucene.analysis.synonym.SynonymMap;
+import org.apache.lucene.analysis.synonym.SynonymMap.Builder;
+import org.apache.lucene.util.CharsRef;
 
 /**
  *
@@ -132,7 +143,7 @@ public class Analizador1 {
 
         sf.reset();
         while (sf.incrementToken()) {
-            System.out.println(sf.getAttribute(CharTermAttribute.class));
+            System.out.println(sf.getAttribute(CharTermAttribute.class) + "\n");
         }
         sf.end();
         sf.close();
@@ -171,13 +182,125 @@ public class Analizador1 {
         return mi_analizador;
     }
 
+    public static Analyzer myAnalyzer(
+        final String language, 
+        final CharArraySet stopwords, 
+        SynonymMap synonymMap, 
+        final CharArraySet commonWords,
+        boolean stop,
+        boolean snowball,
+        boolean shingle,
+        boolean edgeNGramToken,
+        boolean nGramToken,
+        boolean commonGrams,
+        boolean synonym
+    ) {
+        return new Analyzer() {
+            @Override
+            protected TokenStreamComponents createComponents(String string) {
+                // Create the tokenizer (use StandardTokenizer)
+                Tokenizer source = new StandardTokenizer();
+
+                // Create a filter chain (use LowerCaseFilter)
+                TokenStream result = new LowerCaseFilter(source);
+
+                /* Removes stop words from a token stream. */
+                if (stop) result = new StopFilter(result, stopwords);
+
+                /* stem words based on a specific stemmer ex. “indexing”, “indexes”, etc will be stemmed as “index”.*/
+                if (snowball) result = new SnowballFilter(result, language);
+                
+                /*  A ShingleFilter constructs shingles (token n-grams) from a token stream. 
+                In other words, it creates combinations of tokens as a single token. */
+                if (shingle) result = new ShingleFilter(result); 
+
+                /* Tokenizes the given token into n-grams of given size(s). */
+                if (edgeNGramToken) result = new EdgeNGramTokenFilter(result, 2);
+                
+                /* Tokenizes the input into n-grams of the given size(s). 
+                    - handles supplementary characters correctly,
+                    - emits all n-grams for the same token at the same position,
+                    - does not modify offsets,
+                    - sorts n-grams by their offset in the original token first, 
+                        then increasing length (meaning that "abc" will give "a", "ab", "abc", "b", "bc", "c").
+                 */
+                if (nGramToken) result = new NGramTokenFilter(result, 2);
+                
+                /* Construct bigrams for frequently occurring terms while indexing. 
+                   Single terms are still indexed too, with bigrams overlaid. 
+                   This is achieved through the use of PositionIncrementAttribute.setPositionIncrement(int). 
+                   Bigrams have a type of GRAM_TYPE Example:
+                    input:"the quick brown fox"
+                    output:|"the","the-quick"|"brown"|"fox"|
+                    "the-quick" has a position increment of 0 so it is in the same position as "the" "the-quick" has a term.type() of "gram" */
+                
+                if (commonGrams) result = new CommonGramsFilter(result, commonWords);
+                
+                /*
+                 Matches single or multi word synonyms in a token stream. 
+                 This token stream cannot properly handle position increments != 1, 
+                 ie, you should place this filter before filtering out stop words.
+                 */
+                if (synonym) result = new SynonymFilter(result, synonymMap, true);
+                
+                return new TokenStreamComponents(source, result);
+            }
+        };
+    }
+
+
+    public static void testAnalizador(String text) throws IOException{
+        System.out.println("Text: "+ text);
+        System.out.println("Analyzer: " + "\n");
+        List<String> stopWordsList = Arrays.asList("a", "an", "the","is","this","and");
+
+        Builder mybuilder = new SynonymMap.Builder(true);
+        //  Add your synonym rules
+        mybuilder.add(new CharsRef("happy"), new CharsRef("joyful"), true);
+        mybuilder.add(new CharsRef("car"), new CharsRef("automobile"), true);
+        SynonymMap synonymMap = mybuilder.build();
+        
+        List<String> commonWords = Arrays.asList("the","a","that","they");
+
+        Analyzer customAnalyzer = myAnalyzer(
+            "English", 
+            new CharArraySet(stopWordsList, true), 
+            synonymMap, 
+            new CharArraySet(commonWords, true),
+            true, //StopFilter,
+            false, //SnowballFilter
+            false, //ShingleFilter
+            false, //EdgeNGramCommonFilter
+            false, //NGramTokenFilter
+            false, //CommonGramsFilter
+            false //SynonymFilter
+        );
+
+        muestraTexto(customAnalyzer, text);
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        
+        switch(args[0]){
+            case "-1":
+                testAnalizador(args[1]);
+                break;
 
-        analyzerComparison(args[0]);
+            case "-2":
+                analyzerComparison(args[1]);
+                break;
+
+            case "-3":
+                System.out.println("3rd exercise");
+                break;
+            default:
+                System.out.println("Error. Unrecognized option");
+                break;
+        }
 
         /*String cadena = "Ejemplo,  whiteSpace (3+2) \n de analizador + WhiteSpace, \n lucene-7.1.0 \n"
                 + "Analyzer mi_analizador = new Analyzer() {\n"
