ebd9e00df59420c5f1cb2db55eb501cd7fcdef41
eefthymiou <johnefthimiou1002@gmail.com>
2023-10-22 17:01:37 +0200

    conflict

--- Practica3/Practica3.java
+++ Practica3/Practica3.java
@@ -3,12 +3,12 @@ package Practica3;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.Reader;
 import java.io.IOException;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-
 
 
 public class Practica3 {
@@ -21,22 +21,23 @@ public class Practica3 {
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-    
-                    for (CSVRecord csvRecord : csvParser) {
-                        // Access individual fields using csvRecord.get(index)
-                        // For example, csvRecord.get(0) will give you the first field.
-                        String field1 = csvRecord.get(0);
-                        String field2 = csvRecord.get(1);
-                        
-                        // Process your data here
+                    try {
+                        Reader reader = new FileReader(file);
+                        if (!reader.ready()){
+                            System.out.println("Error");
+                        }
+                        CSVReader csvReader = new CSVReader(reader);
+
+
+                        reader.close();
                     }
-                                
-                            
-                    } else {
-                            System.err.println("El directorio no existe o está vacío.");
+                    catch (IOException e) {
+                        System.out.println(e.getMessage());
+                        break;
                     }
+                    
                 }
+            }
         }
     }
 
