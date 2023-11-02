200659b14fb6759c44ea349dd1eb1ee18cad6ec6
Santiago Álvarez <A01640172@itesm.mx>
2023-11-02 09:53:18 +0100

    added incomplete configuration

--- Practica4/EpisodeIndexer.java
+++ Practica4/EpisodeIndexer.java
@@ -1,10 +1,41 @@
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+import import org.apache.lucene.index.IndexWriter;
+
+import java.util.Map;
+import java.util.HashMap;
+
 public class EpisodeIndexer {
 
-	public static void createIndex(String directory){
-		System.out.println("Indexing episodes stored in " + directory);
+	private IndexWriter writer;
+	bool create = false;
+
+	EpisodeIndexer(boolean overwrite){
+		create = overwrite;
+
+		configureIndex();
 	}
 
-	public static void updateIndex(String directory){
-		System.out.println("Updating index with episodes stored in " + directory);
+	void configureIndex(){
+		Map<String,Analyzer> analyzerPerField = new HashMap<String,Analyzer>();
+		analyzerPerField.put("spoken_words", new EnglishAnalyzer());
+		analyzerPerField.put("raw_character_text", new CharacterTextAnalyzer());
+		analyzerPerField.put("title", new EnglishAnalyzer());
+
+		PerFieldAnalyzerWrapper analyzer = new PerFieldAnalyzerWrapper(new WhitespaceAnalyzer(), analyzerPerField);
+		Similarity sim = new ClassicSimilarity();
+
+		IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+		iwc.setSimilarity(sim);
+
+	}
+
+	public static void index(String directory){
+		System.out.println("Indexing episodes stored in " + directory);
 	}
 }
--- Practica4/Practica4.java
+++ Practica4/Practica4.java
@@ -1,14 +1,17 @@
 public class Practica4 {
-    private static EpisodeIndexer indexer;
 
     public static void main(String[] args) {
         if(args[0].equals("index")){
             if(args[1].equals("episodes")){
-                indexer.createIndex(args[2]);
+                EpisodeIndexer indexer = new EpisodeIndexer(true);
+
+                indexer.index(args[2]);
             }
         } else if (args[0].equals("append")){
             if(args[1].equals("episodes")){
-                indexer.updateIndex(args[2]);
+                EpisodeIndexer indexer = new EpisodeIndexer(false);
+
+                indexer.index(args[2]);
             }
         }
     }
