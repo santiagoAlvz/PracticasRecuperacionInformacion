6f68679c447a990150883f4caff59a882ef73375
Santiago Álvarez <A01640172@itesm.mx>
2023-11-02 08:50:11 +0100

    updated

--- Practica4/Practica4.java
+++ Practica4/Practica4.java
@@ -2,12 +2,12 @@ public class Practica4 {
     private static EpisodeIndexer indexer;
 
     public static void main(String[] args) {
-        if(args[0].equals("create")){
-            if(args[1].equals("episodeIndex")){
+        if(args[0].equals("index")){
+            if(args[1].equals("episodes")){
                 indexer.createIndex(args[2]);
             }
-        } else if (args[0].equals("update")){
-            if(args[1].equals("episodeIndex")){
+        } else if (args[0].equals("append")){
+            if(args[1].equals("episodes")){
                 indexer.updateIndex(args[2]);
             }
         }
