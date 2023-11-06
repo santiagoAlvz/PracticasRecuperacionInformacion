83b3b48b85f0928cf2e14aa81f1704e24b44b842
Santiago Álvarez <A01640172@itesm.mx>
2023-11-06 21:31:39 +0100

    indexed fields for capitulosUnidos

--- Practica4/EpisodeIndexer.java
+++ Practica4/EpisodeIndexer.java
@@ -1,6 +1,13 @@
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FloatPoint;
+import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.search.similarities.Similarity;
@@ -9,11 +16,18 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.index.IndexWriter;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
 import java.util.Map;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.io.Reader;
+import java.io.FileReader;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class EpisodeIndexer {
 
@@ -21,8 +35,8 @@ public class EpisodeIndexer {
 	boolean create = false;
 	String indexPath = "./index/episodes";
 
-	EpisodeIndexer(boolean overwrite){
-		create = overwrite;
+	EpisodeIndexer(boolean create){
+		this.create = create;
 
 		try {
 			configureIndex();
@@ -43,15 +57,15 @@ public class EpisodeIndexer {
 		IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
 		iwc.setSimilarity(sim);
 		
-		if(create) {
+		if(this.create) {
 			iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
 		} else {
 			iwc.setOpenMode(IndexWriterConfig.OpenMode.APPEND);
 		}
 		
-		Directory dir = FSDirectory.open(Paths.get(indexPath));
+		Directory dir = FSDirectory.open(Paths.get(this.indexPath));
 		
-		writer = new IndexWriter(dir, iwc);
+		this.writer = new IndexWriter(dir, iwc);
 
 	}
 
@@ -60,6 +74,75 @@ public class EpisodeIndexer {
 		
 		File folder = new File(directory);
 		File[] files = folder.listFiles();
+		
+		for (File file : files) {
+            if (file.isFile()) {
+                try {
+                    Reader reader = new FileReader(file);
+                    Document doc = new Document();
+                    
+                    if (!reader.ready()){
+                        System.out.println("Error");
+                    }
+                    
+                    CSVReader csvReader = new CSVReader(reader);
+                    
+                    String[] nextRecord;
+                    csvReader.readNext();
+                    
+                    while((nextRecord = csvReader.readNext()) != null){
+                    	//add our fields to the lucene doc
+                    	
+                    	//Index it "twice", so the value can be both used to search and be displayed in the results
+                    	doc.add(new IntPoint("episode_id", Integer.parseInt(nextRecord[1])));
+                    	doc.add(new StoredField("episode_id", nextRecord[1]));
+                    	
+                    	doc.add(new TextField("spoken_words", nextRecord[2], Field.Store.YES));
+                    	
+                    	doc.add(new TextField("raw_character_text", nextRecord[3], Field.Store.YES));
+                    	
+                    	doc.add(new FloatPoint("imdb_rating", Float.parseFloat(nextRecord[4])));
+                    	
+                    	doc.add(new IntPoint("imdb_votes", (int)Float.parseFloat(nextRecord[5])));
+                    	
+                    	doc.add(new IntPoint("number_in_season", Integer.parseInt(nextRecord[6])));
+                    	doc.add(new StoredField("number_in_season", nextRecord[6]));
+                    	
+                    	try {                    	
+	                    	Date date = new SimpleDateFormat("yyyy-MM-dd").parse(nextRecord[7]);
+	                    	
+	                    	doc.add(new LongPoint("original_air_date", date.getTime()));
+	                    	doc.add(new StoredField("original_air_date", nextRecord[7]));
+                    	} catch(Exception e) {
+                    		System.out.println(e.getMessage());
+                    	}
+                    	
+                    	doc.add(new IntPoint("original_air_year", Integer.parseInt(nextRecord[8])));
+                    	
+                    	doc.add(new IntPoint("season", Integer.parseInt(nextRecord[9])));
+                    	doc.add(new StoredField("season", nextRecord[9]));
+                    	
+                    	doc.add(new TextField("title", nextRecord[10], Field.Store.YES));
+                    	
+                    	doc.add(new FloatPoint("us_viewers_in_millions", Float.parseFloat(nextRecord[11])));
+                    	doc.add(new StoredField("us_viewers_in_millions", nextRecord[11]));
+                    	
+                    	doc.add(new FloatPoint("views", Float.parseFloat(nextRecord[12])));
+                    	
+                    	writer.addDocument(doc);
+                    }
+                    
+                    csvReader.close();
+                    reader.close();
+                }
+                catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+                catch (CsvValidationException e){
+                    System.out.println(e.getMessage());
+                }        
+            }
+        }
 		
 		close();
 	}
